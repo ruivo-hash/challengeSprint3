d0caa0cb85f63469e8996cb25e5670a4699aaa63
ruivo-hash <maateusb123@gmail.com>
2021-09-04 11:35:29 -0300

    projeto cadastroTutor

---
+++ CadastroTutor/CadastroTutor/src/main/java/br/com/fiap/CadastroTutor/CadastroTutorApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.CadastroTutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroTutorApplication.class, args);
+	}
+
+}
